0281aa248eacc9d61edd3ecd337b10c012857384
JatinMehta007 <mehtajatin045@gmail.com>
2024-10-13 12:41:51 +0530

    completed

--- src/main/grid.tsx
+++ src/main/grid.tsx
@@ -14,46 +14,49 @@ export const Grid=()=>{
     const words = [
         {
           text: "Project's",
-          className:"text-slate-600 sm:text-9xl text-4xl"
+          className:"text-slate-600 md:text-7xl lg:text-9xl  text-4xl"
         },
         {
             text: "link below",
-            className:"text-orange-600 sm:text-4xl "
+            className:"text-orange-600 lg:text-4xl md:text-3xl "
           },
       ];
     return (
         <div className="">
-            <div className="uppercase sm:text-5xl text-3xl font-bold sm:m-20 m-10 text-center sm:text-left text-slate-500 tracking-widest   relative   overflow-hidden">
+            <div className="uppercase md:text-5xl text-3xl font-bold md:m-20 m-10 text-center md:text-left text-slate-500 tracking-widest   relative   overflow-hidden">
                 selected work
             </div>
-            <div className="grid grid-cols-3  text-white sm:gap-4 sm:m-10 m-5 sm:max-w-7xl  sm:w-screen">
+            <div className="grid grid-cols-3  text-white  md:gap-4 lg:m-10 m-5 lg:max-w-7xl ">
                 
-                <div className="col-span-2 border-white rounded-3xl sm:h-[300px] w-56 sm:w-[848px] " >
+                <div className="col-span-2 border-white rounded-3xl  md:h-[300px] w-56 md:w-[500px] lg:w-[848px] " >
                     <a href="https://victor--delta.vercel.app/" target="_blank" rel="noopener noreferrer">
-                    <img src={icon3} alt="Victor" className="sm:rounded-3xl rounded-lg"/>
+                    <img src={icon3} alt="Victor" className="md:rounded-3xl rounded-lg"/>
                     </a>
-                <div className=" col-span-2 mt-6  border-white  rounded-3xl sm:h-[300px] w-56 sm:w-[848px]" >
+
+                <div className=" col-span-2 mt-6  border-white  rounded-3xl md:h-[300px] w-56 md:w-[500px] lg:w-[848px]" >
                 <a href="https://alabayjatinmehta-jatins-projects-a1a570d0.vercel.app/" target="_blank" rel="noopener noreferrer">
-                    <img src={icon1} alt="Alabay" className="sm:rounded-3xl rounded-lg "/>
+                    <img src={icon1} alt="Alabay" className="md:rounded-3xl rounded-lg "/>
                     </a>
+
                 </div>
-                <div className=" col-span-2 sm:mt-32 mt-6 border-white  rounded-3xl sm:h-[300px] w-56 sm:w-[848px]" >
+                
+                <div className=" col-span-2 md:mt-[-29px] lg:mt-32 mt-6 border-white  rounded-3xl md:h-[300px] w-56 md:w-[500px] lg:w-[848px]" >
                 <a href="https://jatinmehta007.github.io/IXIGO-TRAIN-APP-CLONE/" target="_blank" rel="noopener noreferrer">
-                    <img src={icon5} alt="Ixigo" className="sm:rounded-3xl rounded-lg"/>
+                    <img src={icon5} alt="Ixigo" className="md:rounded-3xl rounded-lg"/>
                     </a>
                 </div>
-                <div className="sm:mt-32 mt-10">
+                <div className="lg:mt-32 md:mt-[-70px] mt-10">
                     <TypewriterEffectSmooth words={words}/>
                 </div>
                 </div>
                 
                 <div className="border-white   rounded-3xl ">
                 <a href="https://victor--delta.vercel.app/" target="_blank" rel="noopener noreferrer">
-                <img src={icon4} alt="Victor" className="sm:rounded-3xl rounded-lg " />
+                <img src={icon4} alt="Victor" className="md:rounded-3xl rounded-lg " />
                 </a>
                 <div className="border-white  mt-4 rounded-3xl ">
                 <a href="https://alabayjatinmehta-jatins-projects-a1a570d0.vercel.app/" target="_blank" rel="noopener noreferrer">
-                <img src={icon2} alt="Alabay" className="sm:rounded-2xl rounded-lg " />
+                <img src={icon2} alt="Alabay" className="md:rounded-2xl rounded-lg " />
                 </a>
                 </div>
                 </div>
--- src/main/navbar.tsx
+++ src/main/navbar.tsx
@@ -7,29 +7,30 @@ export const Nav=()=>{
      
     return (
       <div className=" flex items-center justify-center ">
-      <div className="fixed flex top-0 h-14 w-96 sm:h-20 m-6   sm:w-[950px] border-transparent transition duration-1000 ease-in-out bg-gradient-to-r from-gray-900 via-black to-gray-900 rounded-3xl text-slate-200 hover:border hover:border-gray-700">
-          <div  className="sm:m-7 m-4 text-sm sm:text-base uppercase text-slate-600  tracking-wider sm:font-medium">
+      <div className="fixed flex items-center justify-center top-0 h-14 w-96 md:w-[700px] lg:h-20 m-6   lg:w-[950px] border-transparent transition duration-1000 ease-in-out bg-gradient-to-r from-gray-900 via-black to-gray-900 rounded-3xl text-slate-200 hover:border hover:border-gray-700">
+
+          <div  className="lg:m-7 m-4 text-sm md:text-base lg:text-lg  uppercase text-slate-600  tracking-wider lg:font-medium">
           Jatin Mehta
           </div>
 
-          <div className="ml-auto">
+          <div className="ml-auto ">
             <a href="https://github.com/JatinMehta007/Portfolio" target="_blank" rel="noopener noreferrer">
           <button>
-            <img src={github} alt="" className="sm:p-4 h-7 w-7 mt-4 sm:h-20 sm:w-20 sm:mt-0  rounded-full transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-700 border-gray-600 hover:border hover:border-gray-900" />
+            <img src={github} alt="" className="lg:p-4 h-7 mt-2 w-7 md:h-8 md:w-8  lg:h-20 lg:w-20 lg:mt-0  rounded-full transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-700 border-gray-600 hover:border hover:border-gray-900" />
           </button>
           </a>
           </div>
      <div>
         <a href="https://www.linkedin.com/in/jatin-mehta-a70a0025a/" target="_blank" rel="noopener noreferrer">
         <button>    
-        <img src={linkdin} alt="" className="sm:p-4 h-7 w-7 m-4 sm:h-20 sm:w-20 sm:m-0 rounded-full transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-700 border-gray-600 hover:border hover:border-gray-900" />
+        <img src={linkdin} alt="" className="lg:p-4 h-7 m-3 mt-5 lg:m-0  w-7 md:h-8 md:w-8   lg:h-20 lg:w-20 lg:mt-0 rounded-full transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-700 border-gray-600 hover:border hover:border-gray-900" />
         </button>
         </a>
      </div>
      <div>
-        <a href="images/resume.pdf" download target="_blank" rel="noopener noreferrer">
+        <a href="images/Jatin_Resume.pdf" download target="_blank" rel="noopener noreferrer">
         <button>    
-        <img src={resume} alt="" className="sm:p-4 h-7 w-7 mt-4 mr-2 sm:h-20 sm:w-20 sm:m-0 rounded-full transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-700 sm:border-gray-600 sm:hover:border sm:hover:border-gray-900" />
+        <img src={resume} alt="" className="lg:p-4 h-7 mt-2 w-7 md:h-8 md:w-8  mr-2 lg:h-20 lg:w-20 lg:m-0 rounded-full transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-700 lg:border-gray-600 lg:hover:border lg:hover:border-gray-900" />
         </button>
         </a>
      </div>
