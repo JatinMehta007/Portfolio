fb0cebdfe45afefc74c28269d5fcf13ba9edb6fa
Jatin_Mehta <mehtajatin045@gmail.com>
2025-04-26 17:57:12 +0530

    update text-size

--- src/main/svg.tsx
+++ src/main/svg.tsx
@@ -11,7 +11,7 @@ export function SVGMaskEffectDemo() {
       className=" flex items-center justify-center  overflow-hidden mt-5 sm:mt-0 ">
       
           <p
-            className="max-w-4xl mx-auto text-slate-600 text-center p-4 sm:p-0 sm:text-5xl text-3xl  font-bold">
+            className="max-w-4xl mx-auto dark:text-black text-zinc-100 text-center p-4 sm:p-0 sm:text-6xl text-3xl  font-bold">
          "Hi,👋 I'm Jatin Mehta — <FlipWords words={words}/> "
           </p>
         
