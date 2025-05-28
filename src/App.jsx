13054e21c4d1fa970fdac7d1f3a96b99975ebf3f
Jatin_Mehta <mehtajatin045@gmail.com>
2025-05-29 00:37:19 +0530

    change text-color

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import { InfiniteMovingCardsDemo } from "./main/moving-stack";
         <div className="sm:mt-40 mt-28">
         <SVGMaskEffectDemo/>
         </div>
-      <div className="md:text-xl text-xs text-white dark:font-medium text-center m-4 md:m-20 font-normal tracking-wider dark:text-black">
+      <div className="md:text-xl text-xs  dark:font-medium text-center m-4 md:m-20 font-normal tracking-wider  text-gray-300 dark:text-black ">
       A passionate Full-Stack Developer and UI Designer currently in the 3rd year of my B.Tech in Computer Engineering. I specialize in creating responsive, user-friendly web applications with a strong focus on UI/UX design. Eager to apply my skills in real-world scenarios, I’m actively seeking internship opportunities where I can contribute to impactful projects, grow as a developer, and learn from industry professionals.</div>
       <div>
         <Grid/>
