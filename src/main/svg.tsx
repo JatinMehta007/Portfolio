007f9f192ed898382e8798d98a0a002f7a48d4cf
Jatin_Mehta <mehtajatin045@gmail.com>
2025-06-19 20:15:22 +0530

    add words

--- src/main/svg.tsx
+++ src/main/svg.tsx
@@ -1,19 +1,19 @@
 "use client";
 import { FlipWords } from "../components/ui/flipword";
-import React from "react";
+
 
 export function SVGMaskEffectDemo() {
-  const words = ["a full stack developer" , "coder" , "Freelancer","Frontend Developer" ]
+  const words = ["Full stack developer" , "coder" , "Freelancer"]
   return (
     <div > 
 
     <div
-      className=" flex items-center justify-center  overflow-hidden mt-5 sm:mt-0 ">
+      className="relative flex   overflow-hidden mt-5 sm:mt-0 ">
       
-          <p
-            className="max-w-4xl mx-auto dark:text-black text-zinc-100 text-center p-4 sm:p-0 sm:text-6xl text-3xl  font-bold">
-         "Hi,👋 I'm Jatin Mehta — <FlipWords words={words}/> "
-          </p>
+          <div
+            className=" text-white  p-4 sm:p-0 sm:text-5xl text-3xl  font-bold">
+         "Hi,👋 I'm Jatin —<FlipWords words={words}/>"
+          </div>
         
 
           </div>
