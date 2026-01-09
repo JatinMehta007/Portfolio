97c16aeb1b9862aa6f4f8f1510fa15fc9295324d
Jatin_Mehta <mehtajatin045@gmail.com>
2026-01-10 00:19:45 +0530

    add smooth-scroll

---
+++ src/components/ui/scroll.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+
+export default function SmoothWrapper({ children }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      smooth: true,
+      lerp: 0.1,
+      wheelMultiplier: 1.1,
+    });
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
+
+  return <>{children}</>;
+}
