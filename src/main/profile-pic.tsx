6f14b38558578a5465576a2846982f147802b18e
Jatin_Mehta <mehtajatin045@gmail.com>
2026-01-09 23:41:42 +0530

    change profile image

--- src/main/profile-pic.tsx
+++ src/main/profile-pic.tsx
@@ -1,21 +1,29 @@
 "use client";
+import { IconLocation, IconMapPin } from "@tabler/icons-react";
 import { AnimatedTooltip } from "../components/ui/profile";
 
 const people = [
   {
-    id: 1,
+    id : 1,
     name: "Jatin Mehta",
     designation: "Developer",
     image:
-      "https://i.postimg.cc/KvHtCFjC/Whats-App-Image-2025-04-26-at-22-42-37.jpg",
+      "https://i.postimg.cc/DwwNdbQD/avatar.jpg",
   },
 ];
 
 export function ProfilePic() {
   return (
-    <div className="lg:mt-16 mt-20 ml-10 sm:ml-0 relative md:left-16 lg:left-14 xl:left-0 md:mt-16 m-auto xl:pr-[700px] ">
-
+    <div className="lg:mt-16 mt-20 ml-10 sm:ml-0 relative md:left-16 lg:left-14 xl:left-0 md:mt-16 m-auto xl:pr-[700px] flex">
       <AnimatedTooltip items={people} /> 
+      <div className="relative left-6 w-screen ">
+      <p className=" font-semibold text-3xl">Jatin Mehta</p>
+      <div className="flex gap-1 text-xs ">
+      <IconMapPin className="w-[15px]"></IconMapPin>
+      <p className="mt-1">Jaipur,India</p>
+      </div>
+      </div>
+      
     </div>
   );
 }
