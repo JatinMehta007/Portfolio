858dc18c580d9669aef881d11c48a813aa83d71a
Jatin_Mehta <mehtajatin045@gmail.com>
2026-01-13 22:21:50 +0530

    using react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About } from "./files/about.tsx";
+import { Navbar2 } from "./files/navbars.tsx";
+import { Contact } from "./files/contactMe.tsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div>
+    <Navbar2></Navbar2>
+    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
