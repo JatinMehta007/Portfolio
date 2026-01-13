495bc308167959e2dd028d95cfb2c7aa1dabc1f8
Jatin_Mehta <mehtajatin045@gmail.com>
2026-01-13 22:20:29 +0530

    add background color

---
+++ src/files/navbars.tsx
@@ -0,0 +1,23 @@
+import { ThemeToggle } from "../components/ui/theme";
+
+export function Navbar2() {
+    return (
+      <div className="
+        fixed border border-zinc-100 dark:border-none
+        z-50 left-1/2 -translate-x-1/2
+        h-14 px-6 w-[61%]
+        flex items-center justify-between
+         dark:bg-black/20
+        backdrop-blur-xl 
+      ">
+        <img src="https://i.postimg.cc/DwwNdbQD/avatar.jpg" alt="avatar" className="rounded-full w-12 h-12" />
+  
+        <div className="flex font-title gap-10 dark:text-white text-black ">
+          <a>About</a>
+          <a href="/about" >Projects</a>
+          <a href="/contact">Contact me</a>
+          <ThemeToggle></ThemeToggle>
+        </div>
+      </div>
+    );
+  }
---
+++ src/files/projects-detail.tsx
@@ -0,0 +1,7 @@
+export function ProjectDetail(){
+    return(
+        <div>
+            asdf
+        </div>
+    )
+}
