f92104394b4efbf555c8d547611e14c605a446d5
Jatin_Mehta <mehtajatin045@gmail.com>
2026-01-16 23:30:56 +0530

    add flip words

--- src/main/profile-pic.tsx
+++ src/main/profile-pic.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { IconLocation, IconMapPin } from "@tabler/icons-react";
 import { AnimatedTooltip } from "../components/ui/profile";
+import { SVGMaskEffectDemo } from "./svg";
 
 const people = [
   {
@@ -17,11 +18,12 @@ export function ProfilePic() {
     <div className="lg:mt-16 mt-20 ml-10 sm:ml-0 relative md:left-16 lg:left-14 xl:left-0 md:mt-16 m-auto xl:pr-[700px] flex">
       <AnimatedTooltip items={people} /> 
       <div className="relative left-6 w-screen ">
-      <p className=" font-semibold text-3xl">Jatin Mehta</p>
+      <p className=" font-semibold text-3xl dark:text-zinc-300 text-black">Jatin Mehta</p>
       <div className="flex gap-1 text-xs ">
       <IconMapPin className="w-[15px]"></IconMapPin>
       <p className="mt-1">Jaipur,India</p>
       </div>
+      <SVGMaskEffectDemo></SVGMaskEffectDemo>
       </div>
       
     </div>
