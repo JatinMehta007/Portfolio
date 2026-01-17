bb5888af5cbd3d000a556e3d638acd3c77489cf8
Jatin_Mehta <mehtajatin045@gmail.com>
2026-01-17 22:27:16 +0530

    add radix theme to the main appbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,25 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { About } from "./files/about.tsx";
+import { ProjectDetails } from "./files/project-details.tsx";
 import { Navbar2 } from "./files/navbars.tsx";
 import { Contact } from "./files/contactMe.tsx";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Theme>
     <div>
     <Navbar2></Navbar2>
     </div>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<ProjectDetails />} />
         <Route path="/contact" element={<Contact></Contact>}></Route>
       </Routes>
     </BrowserRouter>
+    </Theme>
   </StrictMode>
 );
