8161875e1b4cfdaaaaeb8fcba81166b2a9b79012
Jatin_Mehta <mehtajatin045@gmail.com>
2026-01-21 18:36:43 +0530

    make mobile reponsive

--- src/files/navbars.tsx
+++ src/files/navbars.tsx
@@ -4,17 +4,18 @@ export function Navbar2() {
     return (
       <div className="
         fixed border border-zinc-100 dark:border-none
-        z-50 left-1/2 -translate-x-1/2
-        h-14 px-6 w-[61%]
+        z-50 lg:left-1/2 lg:-translate-x-1/2
+        h-16 px-6 lg:w-[61%]
+         w-full
         flex items-center justify-between
          dark:bg-black/20
         backdrop-blur-xl 
       ">
         <a href="/">
-        <img src="https://i.postimg.cc/DwwNdbQD/avatar.jpg" alt="avatar" className="rounded-full w-12 h-12 cursor-pointer" />
+        <img src="https://i.postimg.cc/DwwNdbQD/avatar.jpg" alt="avatar" className="rounded-full h-10 w-10 md:w-12 md:h-12 cursor-pointer" />
         </a>
   
-        <div className="flex font-title gap-10 dark:text-white text-black ">
+        <div className="flex font-title gap-3 md:gap-10 dark:text-white text-black ">
           <a href="/contact">Contact me</a>
           <ThemeToggle></ThemeToggle>
         </div>
